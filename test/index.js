3f1de6facb14a35ba75ba2d8083f3e857336e331
Nickolay Alexandrov <nick.alexandrov@gmail.com>
2015-06-23 12:18:20 +0300

    windows support

---
+++ test/index.js
@@ -0,0 +1,11 @@
+var WmiClient = require('../');
+
+var wmi = new WmiClient({
+    username: null,
+    password: null,
+    host: '127.0.0.1'
+});
+
+wmi.query('SELECT * FROM Win32_Process', function (err, result) {
+    console.log(err || result);
+});
